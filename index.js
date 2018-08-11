9f901173fa021e42cca966d0adfc077a6808850d
frealis <36343653+frealis@users.noreply.github.com>
2018-08-11 15:46:13 -0400

    Changed input to drop-down menus, results for cell X and Y correctly display on test page, added about 10 elements to postgres database

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ const PORT = process.env.PORT || 5000
 
 const { Pool } = require('pg');
 const pool = new Pool({
-  // connectionString: 'postgres://cdoeymfxwzrlil:f27c39881e1ccce41763fee20b1bac54eb1d674d9cbcb63da79e6922ef3a6135@ec2-23-23-216-40.compute-1.amazonaws.com:5432/d3v5ah20dp9gqv',
   connectionString: process.env.DATABASE_URL,
   ssl: true
 });
@@ -17,8 +16,32 @@ express()
   .set('views', path.join(__dirname, 'views'))
   .set('view engine', 'ejs')
 
-  .get('/', (req, res) => res.render('pages/index'))
-  .get('/test', (req, res) => res.render('pages/test'))
+  .get('/', (req, res) => res.render('pages/index', {
+    result_en_a: '',
+    result_an_a: '',
+    result_sep_a: '',
+    result_en_b: '',
+    result_an_b: '',
+    result_sep_b: ''
+  }))
+  .get('/test', async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const x = await client.query('SELECT * FROM periodic_table');
+      res.render('pages/test', {
+        result_en_x: '',
+        result_an_x: '',
+        result_sep_x: '',
+        result_en_y: '',
+        result_an_y: '',
+        result_sep_y: '',
+        element: x.rows
+      });
+      console.log(x.rows);
+      client.release();
+    } 
+    catch (err) {console.error(err); res.send("Error " + err);}
+  })
   .get('/db', async (req, res) => {
     try {
       const client = await pool.connect()
@@ -32,23 +55,38 @@ express()
     catch (err) {console.error(err); res.send("Error " + err);}
   })
 
-  .post('/', urlencodedParser, function (req, res) {
-    res.send('welcome, ' + req.body.input_1)
+  .post('/', urlencodedParser, async (req, res) => {
+    try {
+      const client = await pool.connect()
+      const result_a = await client.query('SELECT * FROM periodic_table WHERE atomicnumber=' + req.body.input_a);
+      const result_b = await client.query('SELECT * FROM periodic_table WHERE atomicnumber=' + req.body.input_b);
+      res.render('pages/index', {
+        result_en_a: result_a.rows[0].elementname,
+        result_an_a: result_a.rows[0].atomicnumber,
+        result_sep_a: result_a.rows[0].sep,
+        result_en_b: result_b.rows[0].elementname,
+        result_an_b: result_b.rows[0].atomicnumber,
+        result_sep_b: result_b.rows[0].sep
+      });
+      client.release();
+    } 
+    catch (err) {console.error(err); res.send("Error " + err);}
   })
-
-  // .post('/test', urlencodedParser, function (req, res) {
-  //   res.send('welcome, ' + req.body.input_a)
-  // })
-
   .post('/test', urlencodedParser, async (req, res) => {
     try {
       const client = await pool.connect()
-      const result = await client.query('SELECT * FROM periodic_table WHERE atomicnumber=' + req.body.input_a);
-      // console.log(req.body.input_a);
-      console.log(result.rows[0]);
-      console.log(result.rows[0].atomicnumber);
-      res.render('pages/test', {result: result.rows[0].atomicnumber});
-      // res.send('output: ' + result.rows[0].atomicnumber);
+      const result_x = await client.query(`SELECT * FROM periodic_table WHERE elementname='${req.body.input_x}'`);
+      const result_y = await client.query(`SELECT * FROM periodic_table WHERE elementname='${req.body.input_y}'`);
+      const x = await client.query('SELECT * FROM periodic_table');
+      res.render('pages/test', {
+        result_en_x: result_x.rows[0].elementname,
+        result_an_x: result_x.rows[0].atomicnumber,
+        result_sep_x: result_x.rows[0].sep,
+        result_en_y: result_y.rows[0].elementname,
+        result_an_y: result_y.rows[0].atomicnumber,
+        result_sep_y: result_y.rows[0].sep,
+        element: x.rows
+      });
       client.release();
     } 
     catch (err) {console.error(err); res.send("Error " + err);}
